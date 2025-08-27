730593cc7f73237857beab89ed12c721780eb0ec
Halima Muktar <halimamuk1307@gmail.com>
2025-08-27 08:57:35 +0300

    adding properties/views.py, properties/urls.py, alx_backend_caching_property_listings/urls.py

--- properties/views.py
+++ properties/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from django.views.decorators.cache import cache_page
 from django.http import JsonResponse
-from .models import Property  # assuming you already have a Property model
+from .models import Property
 
-# Cache this view for 15 minutes (900 seconds)
+# Cache this view for 15 minutes
 @cache_page(60 * 15)
 def property_list(request):
     properties = Property.objects.all().values(
         "id", "title", "description", "price", "location"
     )
-    return JsonResponse(list(properties), safe=False)
+    return JsonResponse({"data": list(properties)}, safe=False)
