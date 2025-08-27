eb98ce12831b6618cc5349467fc82e8e7ca0ea56
Halima Muktar <halimamuk1307@gmail.com>
2025-08-27 19:11:27 +0300

    adding properties/utils.py

--- properties/utils.py
+++ properties/utils.py
@@ -1,5 +1,10 @@
 from django.core.cache import cache
 from .models import Property
+import logging
+from django_redis import get_redis_connection
+
+logger = logging.getLogger(__name__)
+
 
 def get_all_properties():
     """
@@ -7,7 +12,6 @@ def get_all_properties():
     Cache key: 'all_properties'
     TTL: 1 hour
     """
-    # Try to fetch from Redis
     properties = cache.get('all_properties')
     if properties is None:
         # Cache miss → query DB
@@ -17,3 +21,28 @@ def get_all_properties():
         # Store in Redis for 1 hour
         cache.set('all_properties', properties, 3600)
     return properties
+
+
+def get_redis_cache_metrics():
+    """
+    Retrieve Redis cache hit/miss stats and calculate hit ratio.
+    """
+    conn = get_redis_connection("default")
+    info = conn.info()
+
+    hits = info.get("keyspace_hits", 0)
+    misses = info.get("keyspace_misses", 0)
+    total = hits + misses
+
+    hit_ratio = (hits / total) if total > 0 else 0.0
+
+    metrics = {
+        "hits": hits,
+        "misses": misses,
+        "hit_ratio": round(hit_ratio, 4),  # 4 decimal precision
+    }
+
+    # Log for debugging
+    logger.info(f"Redis Cache Metrics: {metrics}")
+
+    return metrics
